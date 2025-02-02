199bdd2010583522318568cda995018e65d527ed
irfan-nodesure <irfan.gouri@nodesure.in>
2025-02-02 23:20:59 +0530

    changes

--- backend/routes/indexRoutes.js
+++ backend/routes/indexRoutes.js
@@ -1,18 +1,10 @@
 const express = require('express');
 
-const userRoutes = require('./userRoutes');
 const patientRoutes = require('./patient.routes');
-
-const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 // Centralizing all the routes in one file
-router.use(
-  '/user',
-  userRoutes,
-);
-
 router.use(
   '/patient',
   patientRoutes,
