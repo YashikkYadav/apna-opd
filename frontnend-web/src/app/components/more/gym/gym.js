c1b7b08c7b7f4e98ad748021277e13a0a65631ec
lakshyapanchal <panchallakshya64@gmail.com>
2025-04-04 00:35:52 +0530

    minor

--- frontnend-web/src/app/components/more/gym/gym.js
+++ frontnend-web/src/app/components/more/gym/gym.js
@@ -101,6 +101,7 @@ const Gym = () => {
                     alt="Location Icon"
                   />
                 }
+                value={location}
               >
                 {locations.map((location) => (
                   <Select.Option key={location} value={location}>
