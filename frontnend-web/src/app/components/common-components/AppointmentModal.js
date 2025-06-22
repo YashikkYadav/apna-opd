d025c9b90b7c9df80be7e1e9a271fc89c962ea97
Shashwat Awasthi <shashlko2002@gmail.com>
2025-06-22 17:09:49 +0530

    FIX: add appointment in doctor panel

--- frontnend-web/src/app/components/common-components/AppointmentModal.js
+++ frontnend-web/src/app/components/common-components/AppointmentModal.js
@@ -32,6 +32,7 @@ const AppointmentModal = ({ doctorDetails, visible, onClose }) => {
   ];
 
   useEffect(() => {
+    console.log(doctorDetails);
     if (visible) {
       document.body.style.overflow = "hidden";
       fetchLocations();
@@ -57,7 +58,7 @@ const AppointmentModal = ({ doctorDetails, visible, onClose }) => {
     try {
       setIsLoading(true);
       const { locations } = await axiosInstance.get(
-        `/appointment/${doctorDetails.doctorId}/locations`
+        `/appointment/${doctorDetails.doctorId._id}/locations`
       );
       if (locations) {
         setLocations(locations);
@@ -74,7 +75,7 @@ const AppointmentModal = ({ doctorDetails, visible, onClose }) => {
     try {
       setIsLoading(true);
       const { dates } = await axiosInstance.get(
-        `/appointment/${doctorDetails.doctorId}/${locationId}/dates`
+        `/appointment/${doctorDetails.doctorId._id}/${locationId}/dates`
       );
       if (dates) {
         setAvailableDates(dates);
@@ -91,7 +92,7 @@ const AppointmentModal = ({ doctorDetails, visible, onClose }) => {
     try {
       setIsLoading(true);
       const { timeSlots } = await axiosInstance.get(
-        `/appointment/${doctorDetails.doctorId}/${locationId}/date/${date}`
+        `/appointment/${doctorDetails.doctorId._id}/${locationId}/date/${date}`
       );
       if (timeSlots) {
         setTimeSlots(timeSlots);
@@ -176,7 +177,7 @@ const AppointmentModal = ({ doctorDetails, visible, onClose }) => {
       let response;
       if (formData.appointmentType === "offline") {
         response = await axiosInstance.post(
-          `/appointment/${doctorDetails.doctorId}/book-appointment`,
+          `/appointment/${doctorDetails.doctorId._id}/book-appointment`,
           {
             phoneNumber: formData.phoneNumber,
             date: formData.date,
@@ -188,7 +189,7 @@ const AppointmentModal = ({ doctorDetails, visible, onClose }) => {
         );
       } else {
         response = await axiosInstance.post(
-          `/appointment/${doctorDetails.doctorId}/book-appointment`,
+          `/appointment/${doctorDetails.doctorId._id}/book-appointment`,
           {
             phoneNumber: formData.phoneNumber,
             email: formData.email,
