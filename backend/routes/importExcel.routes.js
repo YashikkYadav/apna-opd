6978326a6e3fa12003d7fc99d02db889937794be
RockJatin777 <rockjatin0748@gmail.com>
2025-08-02 00:13:20 +0530

    coomit by Jitin

--- backend/routes/importExcel.routes.js
+++ backend/routes/importExcel.routes.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const multer = require("multer");
-const importExcel =  require("../controllers/importExcel.controller.js")
+const {importExcel} =  require("../controllers/importExcel.controller.js")
 
 const importExcelRouter = express.Router();
 const upload = multer({ dest: "uploads/" });
 
-router.post("/import-excel", upload.single("file"), importExcel);
+importExcelRouter.post("/import-excel", upload.single("file"), importExcel);
 
 module.exports = importExcelRouter;
