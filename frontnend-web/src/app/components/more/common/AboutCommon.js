48667a9dc4880ca5cbde243b6e87b6fb2e61781f
RockJatin777 <rockjatin0748@gmail.com>
2025-08-14 12:22:09 +0530

    commit by jitin

--- frontnend-web/src/app/components/more/common/AboutCommon.js
+++ frontnend-web/src/app/components/more/common/AboutCommon.js
@@ -6,41 +6,41 @@ const AboutCommon = ({ profileData, serviceType }) => {
     <div className="max-w-[1270px] px-[15px] sm:px-[30px] mx-auto pt-[60px] md:pt-[120px] pb-[43px]">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-0">
         <div className="flex flex-col justify-center">
-          <h1 className="title-48 mb-[24px]">About {profileData.name}</h1>
+          <h1 className="title-48 mb-[24px]">About {profileData?.name}</h1>
           <p className="text-[#808080] text-base mb-[40px]">
-            {profileData.about || 
+            {profileData?.about || 
               `This is a premium ${serviceType} service provider offering high-quality services to meet your needs. With a focus on customer satisfaction and professional excellence, we strive to deliver the best experience possible.`}
           </p>
           <div className="flex flex-col gap-[32px]">
-            {profileData.certifications && (
+            {profileData?.certifications && (
               <div>
                 <h5 className="title-24 text-[#2E2E2E]">Certifications</h5>
                 <p className="text-base text-[#808080]">
-                  {profileData.certifications}
+                  {profileData?.certifications}
                 </p>
               </div>
             )}
             
-            {profileData.establishment && (
+            {profileData?.establishment && (
               <div>
                 <h5 className="title-24 text-[#2E2E2E]">
                   Established Since
                 </h5>
                 <p className="text-base text-[#808080]">
-                  {profileData.establishment}
+                  {profileData?.establishment}
                 </p>
               </div>
             )}
             
-            {profileData.facilities && (
+            {profileData?.facilities && (
               <div>
                 <h5 className="title-24 text-[#2E2E2E]">Facilities</h5>
                 <ul className="list-disc pl-5 mt-2">
-                  {Array.isArray(profileData.facilities) ? 
-                    profileData.facilities?.map((facility, index) => (
+                  {Array.isArray(profileData?.facilities) ? 
+                    profileData?.facilities?.map((facility, index) => (
                       <li key={index} className="text-base text-[#808080]">{facility}</li>
                     )) : 
-                    <li className="text-base text-[#808080]">{profileData.facilities}</li>
+                    <li className="text-base text-[#808080]">{profileData?.facilities}</li>
                   }
                 </ul>
               </div>
